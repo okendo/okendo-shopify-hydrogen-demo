7d228e2dd83318ba50c0f9fe81f9215441cc7fb5
Zwyx <29386932+Zwyx@users.noreply.github.com>
2024-01-06 15:02:46 +0800

    Update for new version of demo store (#24)
    * Recreate the demo store following loads of changes from Shopify
    
    Run `npm create @shopify/hydrogen@latest`
    
    * Disable ESLint rule `prettier/prettier`
    
    Prettier is run automatically when we save... having red squiggles everywhere for formatting issues is worse than sticking rusty forks in my legs.
    
    * Add Okendo's widgets

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 > **Note**
 > This demo store is built on Hydrogen v2, based on Remix. If you are using a store built with the now deprecated Hydrogen 1, please see [here](https://github.com/okendo/okendo-shopify-hydrogen-demo/tree/hydrogen-1-archive).
 
+> Note: there have been multiple versions of Shopify's Hydrogen demo store. If you project is based on an old version of it, consult our version history to find out how to add Okendo to it.
+
 # Demo store for the Okendo Hydrogen components
 
 Please read the [documentation of the `@okendo/shopify-hydrogen` library](https://www.npmjs.com/package/@okendo/shopify-hydrogen).
@@ -9,11 +11,12 @@ This demo store is simply the `demo-store` template [provided by Shopify](https:
 
 ## Requirements
 
-- Your store must be running Okendo Widget Plus - Our newer widget offering. Upgrading is free.
-- Node.js version 16.14.0 or higher.
+- Your store must be running Okendo Widget Plus – our newer widget offering. Upgrading is free.
+- Node.js version `18.0.0` or higher.
 
 ## Start the demo store locally
 
 ```bash
+npm ci
 npm run dev
 ```
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,5 +1,6 @@
-import type {EntryContext} from '@shopify/remix-oxygen';
 import {RemixServer} from '@remix-run/react';
+import {createContentSecurityPolicy} from '@shopify/hydrogen';
+import type {EntryContext} from '@shopify/remix-oxygen';
 import isbot from 'isbot';
 import {renderToReadableStream} from 'react-dom/server';
 
@@ -9,9 +10,38 @@ export default async function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
+  const {nonce, header, NonceProvider} = createContentSecurityPolicy({
+    defaultSrc: [
+      "'self'",
+      'localhost:*',
+      'https://cdn.shopify.com',
+      'https://d3hw6dc1ow8pp2.cloudfront.net',
+      'https://d3g5hqndtiniji.cloudfront.net',
+      'data:',
+    ],
+    styleSrc: [
+      "'self'",
+      "'unsafe-inline'",
+      'https://cdn.shopify.com',
+      'localhost:*',
+      'https://d3hw6dc1ow8pp2.cloudfront.net',
+    ],
+    connectSrc: [
+      "'self'",
+      'https://monorail-edge.shopifysvc.com',
+      'localhost:*',
+      'ws://localhost:*',
+      'ws://127.0.0.1:*',
+      'https://api.okendo.io',
+    ],
+  });
+
   const body = await renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} />,
+    <NonceProvider>
+      <RemixServer context={remixContext} url={request.url} />
+    </NonceProvider>,
     {
+      nonce,
       signal: request.signal,
       onError(error) {
         // eslint-disable-next-line no-console
@@ -26,6 +56,8 @@ export default async function handleRequest(
   }
 
   responseHeaders.set('Content-Type', 'text/html');
+  responseHeaders.set('Content-Security-Policy', header);
+
   return new Response(body, {
     headers: responseHeaders,
     status: responseStatusCode,
